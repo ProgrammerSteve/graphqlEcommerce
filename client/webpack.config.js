e3a71ed9990e92ebf38413c228573e6d69b133c9
Steven Bautista <stevenmb1995@gmail.com>
2025-03-08 21:14:32 -0600

    Setting up typescript and webpack config files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   experiments: {
     topLevelAwait: true,
   },
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: path.join(__dirname, "src", "index.tsx"),
   output: {
     path: path.resolve(__dirname, "..", "server", "public"),
   },
@@ -13,20 +13,34 @@ module.exports = {
   stats: {
     children: true,
   },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
   module: {
     rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: [
+              "@babel/preset-env", 
+              "@babel/preset-react", 
+              "@babel/preset-typescript"
+            ],
           },
         },
       },
       {
-        test: /\.js$/,
+        test: /\.(js|ts)$/,
         enforce: "pre",
         use: ["source-map-loader"],
       },
