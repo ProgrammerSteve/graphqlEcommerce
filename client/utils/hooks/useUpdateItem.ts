5e496caffab42e23884a0f4bc9b8b49c3658e2ac
Steven Bautista <stevenmb1995@gmail.com>
2025-04-20 22:44:07 -0500

    made the useUpdateItem hook into typescript

---
+++ client/utils/hooks/useUpdateItem.ts
@@ -0,0 +1,36 @@
+import { useMutation } from "@apollo/client";
+import { UPDATE_ITEM } from "../gqlQueries/mutations";
+import { GET_ITEMS } from "../gqlQueries/queries";
+import { Item } from "../../src/types";
+
+export const useUpdateItem = (itemState: Item): [() => Promise<void>] => {
+  const [updateItem, { data, loading, error }] = useMutation(UPDATE_ITEM, {
+    refetchQueries: [{ query: GET_ITEMS }],
+    variables: {
+      id: itemState.id,
+      name: itemState.name,
+      src: itemState.src,
+      price: itemState.price,
+      alt: itemState.alt,
+      stock: itemState.stock,
+      description: itemState.description,
+      length: itemState.length,
+      width: itemState.width,
+      height: itemState.height,
+      weight: itemState.weight,
+      category: itemState.category,
+      discontinued: itemState.discontinued,
+    },
+  });
+
+  const handleUpdate = async (): Promise<void> => {
+    try {
+      await updateItem();
+    } catch (err) {
+      console.log("try catch error:", err);
+      console.log("apolloError:", error);
+    }
+  };
+
+  return [handleUpdate];
+};
