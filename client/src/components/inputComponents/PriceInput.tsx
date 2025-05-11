a922b778ffd18cdbd582fb958b3564edeea7362b
Steven Bautista <stevenmb1995@gmail.com>
2025-05-10 23:03:21 -0500

    Converted PriceInput into tsx

---
+++ client/src/components/inputComponents/PriceInput.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import InputWrapper from "./InputWrapper";
+import { Item } from "../../types";
+
+interface PriceInputProps {
+  title: string;
+  price: number;
+  handlePrice: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  itemState: Item;
+  newItem?: boolean;
+}
+
+const PriceInput: React.FC<PriceInputProps> = ({
+  title,
+  price,
+  handlePrice,
+  itemState,
+  newItem = false,
+}) => {
+  let priceString =
+    price !== undefined
+      ? price.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        })
+      : "$0.00";
+  return (
+    <InputWrapper
+      wrapperTitle={title}
+      wrapperValue={priceString}
+      itemState={itemState}
+      newItem={newItem}
+    >
+      <input
+        type="number"
+        value={price}
+        min="0"
+        step="0.01"
+        onChange={handlePrice}
+        className="w-full h-full text-black pl-2 text-sm"
+      />
+    </InputWrapper>
+  );
+};
+
+export default PriceInput;
