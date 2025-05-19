e8d5e07b19184fa3bd0e3ae6acdc7b7d010a9e22
Steven Bautista <stevenmb1995@gmail.com>
2025-05-18 22:29:55 -0500

    Converted input components index file into ts

---
+++ client/src/components/inputComponents/index.ts
@@ -0,0 +1,7 @@
+export { default as BooleanInput } from "./BooleanInput";
+export { default as FloatInput } from "./FloatInput";
+export { default as InputWrapper } from "./InputWrapper";
+export { default as IntegerInput } from "./IntegerInput";
+export { default as PriceInput } from "./PriceInput";
+export { default as TextAreaInput } from "./TextAreaInput";
+export { default as TextInput } from "./TextInput";
