c9c7a9e13e9b51050e413d3a40223fd144907483
Steven Bautista <stevenmb1995@gmail.com>
2025-06-14 18:59:03 -0500

    Converted ItemDisplay into tsx

---
+++ client/src/pages/itemDisplay/ItemDisplay.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ItemPreview from "../../components/itemPreview/ItemPreview";
+import { processedData } from "../../../utils/filterSorting";
+import { useNavigate } from "react-router-dom";
+import { ItemDisplayProps } from "../../types";
+
+const ItemDisplay: React.FC<ItemDisplayProps> = ({
+  loading,
+  error,
+  data,
+  sortOption,
+  textSearch,
+  minPrice,
+  maxPrice,
+  showSideMenu,
+  toggleSideMenu,
+}) => {
+  const navigate = useNavigate();
+  
+  const handleNavigate = (path: string) => {
+    if (showSideMenu) toggleSideMenu();
+    navigate(path);
+  };
+  
+  return (
+    <div className="box-content w-full grow overflow-y-scroll scrollbar-hide grid grid-cols-1 sm:grid-cols-2 justify-center gap-3 sm:gap-2 md:gap-4">
+      {!loading &&
+        !error &&
+        data &&
+        processedData(data, sortOption, textSearch, minPrice, maxPrice).map(
+          (item) => (
+            <ItemPreview
+              item={item}
+              key={item.id}
+              handleNavigate={handleNavigate}
+            />
+          )
+        )}
+    </div>
+  );
+};
+
+export default ItemDisplay;
