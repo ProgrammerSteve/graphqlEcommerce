60eec7d0164a949d1ce0311d69ab36916f11542d
Steven Bautista <stevenmb1995@gmail.com>
2025-06-14 21:54:25 -0500

    deleting unused files

--- client/src/components/svgComponents/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ConfirmSvg } from "./ConfirmSvg.jsx";
-export { default as EditSvg } from "./EditSvg.jsx";
-export { default as CloseSvg } from "./CloseSvg.jsx";
--- client/utils/hooks/useGetNewItem.js
+++
@@ -1,8 +0,0 @@
-import { useReducer } from "react";
-import { defaultInitialState } from "../itemReducer/itemState";
-import { itemReducer } from "../itemReducer/itemReducer";
-
-export const useGetNewItem = () => {
-  const [itemState, dispatch] = useReducer(itemReducer, defaultInitialState);
-  return { itemState, dispatch };
-};
