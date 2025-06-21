715928598ee8a3c8634ee1934b2c19533ee362d8
Steven Bautista <stevenmb1995@gmail.com>
2025-06-20 21:12:17 -0500

    Converted TextFilter into tsx

---
+++ client/src/components/navBarComponents/TextFilter.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface TextFilterProps {
+  text: string;
+  handleText: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const TextFilter: React.FC<TextFilterProps> = ({ text, handleText }) => {
+  return (
+    <div className="bg-gray-800 px-2 lg:px-3 xl:px-4 grid items-center py-2 md:py-4 rounded-lg border-solid border-2 border-gray-600">
+      <h2 className="text-white block select-none">Search</h2>
+
+      <input
+        type="text"
+        value={text}
+        onChange={handleText}
+        className="block md:w-28 mdlg:w-32 lg:w-38 xl:w-48 2xl:w-56 bg-gray-600 text-white rounded-md px-3"
+      />
+    </div>
+  );
+};
+
+export default TextFilter;
